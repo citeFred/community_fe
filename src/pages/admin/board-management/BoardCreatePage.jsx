62c2b96851d28655d63e146d0664917abd5c3147
citeFred <citefred@gmail.com>
2026-01-29 15:40:19 +0900

    feat: 기초 관리자 페이지(게시판 생성 등) 컴포넌트 및 페이지 구성

---
+++ src/pages/admin/board-management/BoardCreatePage.jsx
@@ -0,0 +1,9 @@
+function BoardCreatePage() {
+    return (
+        <div>
+            게시판 생성 폼 페이지
+        </div>
+    )
+}
+
+export default BoardCreatePage;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 import BoardListPage from "../pages/boards/BoardListPage";
+import AdminPage from "../pages/admin/AdminPage";
+import BoardManagementPage from "../pages/admin/board-management/BoardManagementPage";
+import BoardCreatePage from "../pages/admin/board-management/BoardCreatePage";
 
 function AppRoutes() {
     return (
         <Routes>
+            {/* home */}
             <Route path="/" element={<HomePage />} />
+
+            {/* admin */}
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/admin/boards" element={<BoardManagementPage />} />
+            <Route path="/admin/boards/create" element={<BoardCreatePage />} />
+            
             <Route path="/boards" element={<BoardListPage />} />
         </Routes>
     )
