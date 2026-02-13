f192df9e34c4bba72c4cca0b7c10d6ba83bcac41
citeFred <citefred@gmail.com>
2026-02-13 10:28:20 +0900

    feat: 챗봇 - 페이지 라우터 생성 및 API 요청부 생성

---
+++ src/pages/chatbot/ChatRoomListPage.jsx
@@ -0,0 +1,10 @@
+function ChatRoomListPage() {
+  return (
+    <div>
+      <h1>Chat Room List</h1>
+      {/* Additional content will go here */}
+    </div>
+  );
+}
+
+export default ChatRoomListPage;
---
+++ src/pages/chatbot/chat/ChatPage.jsx
@@ -0,0 +1,10 @@
+function ChatPage() {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      {/* Additional content will go here */}
+    </div>
+  );
+}
+
+export default ChatPage;
